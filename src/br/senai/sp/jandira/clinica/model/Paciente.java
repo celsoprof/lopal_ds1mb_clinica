634697e9b7566d98671cca1f691bc582501ead3f
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-02-10 11:25:40 -0300

    Criação dos atributos e assinaturas dos métodos na classe Paciente.

--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -1,4 +1,35 @@
 package br.senai.sp.jandira.clinica.model;
 
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class Paciente {
+
+    String nome;
+    double peso;
+    double altura;
+    String genero;
+    String telefone;
+    LocalDate dataNascimento;
+
+    public void marcarConsulta(){
+
+    }
+
+    public void calcularIdade() {
+
+    }
+
+    public void calcularImc() {
+
+    }
+
+    public void classificarImc(){
+
+    }
+
+    public void exibirDados() {
+
+    }
+
 }
